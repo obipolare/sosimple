d18d6c9d9f98cd4bddfad7b78f4de6a244698a9a
Adrian <codarko15@gmail.com>
2020-08-26 14:32:27 -0500

    writing a little bit

--- component/scripts.js
+++ component/scripts.js
@@ -1,14 +1,3 @@
-const headerlist = {
-    props: ['url', 'name'],
-    template: `#headerlist-template`
-}
-
-const articles = {
-    props: ['description', 'title', 'image'],
-    template: `#articles-template`
-}
-
-
 new Vue({
     el: '#app',
     data: {
@@ -26,7 +15,7 @@ new Vue({
             id: 3
         }, {
             title: 'click to watch any video wwy',
-            url: 'https://www.youtube.com/watch?v=38zftiUmlUo',
+            url: 'https://www.youtube.com/watch?v=0obTqOhvJkg',
             id: 4
         }],
         articles: [{
@@ -44,11 +33,12 @@ new Vue({
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ad iusto tempore doloribus unde possimus? Quae sunt libero repellat recusandae dolores iure ipsa perspiciatis autem magnam nisi, veritatis quas veniam.',
             image: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/f07c1535-c014-4636-b738-4a9af08f7e0d/d81lrtl-b16fe693-d0ea-4ce8-8205-cf12f20ba85b.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvZjA3YzE1MzUtYzAxNC00NjM2LWI3MzgtNGE5YWYwOGY3ZTBkXC9kODFscnRsLWIxNmZlNjkzLWQwZWEtNGNlOC04MjA1LWNmMTJmMjBiYTg1Yi5qcGcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.zmaN0WcsIbMWtZpJxuAfwLFzuI7DHjSHK8ni94SgUvQ',
             id: 3
-        }, ]
+        }, ],
+        search: "",
+
     },
     components: {
-        headerlist,
         articles
-    },
+    }
 
 })
